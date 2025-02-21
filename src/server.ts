5c0bb35d643b120b52a3178338f03b1071c9a458
Rafael Dias Zendron <mmmarckos@gmail.com>
2025-02-21 13:23:19 -0300

    fix(server): improve server configuration

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,45 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import { environment } from "./config/environment";
-import { linkedinRoutes } from "./routes/linkedin";
 import { CacheService } from "./services/CacheService";
+import { setupSwagger } from "./swagger";
+import passport from 'passport';
+import './config/passport'; // Certifique-se de importar a configuração do Passport
 
-dotenv.config();
+dotenv.config(); // Carregando as variáveis de ambiente
 
-console.log("\n=== Iniciando Servidor ===");
-console.log("Ambiente:", {
-  NODE_ENV: environment.nodeEnv,
-  PORT: environment.port,
-  REDIS_HOST: environment.redis.host,
-});
+// Agora você pode importar o environment
+import { environment } from "./config/environment"; // Importando as variáveis de ambiente
+import { linkedinRoutes } from "./routes/linkedin";
 
 const app = express();
 const cache = new CacheService();
+
+// Configurações do middleware
+app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
+
+// Configuração das rotas
+app.use("/api/linkedin", linkedinRoutes);
+
+// Configuração do Swagger
+setupSwagger(app);
+
+app.get("/", (_req, res) => {
+  res.redirect("/api-docs"); // Redireciona para a documentação do Swagger
+});
 
 const startServer = async (): Promise<void> => {
   try {
     await cache.waitForConnection();
 
-    app.use(
-      cors({
-        origin: environment.cors.origin,
-        credentials: true,
-      }),
-    );
-    app.use(express.json());
-
-    // Configuração das rotas
-    app.use("/api/linkedin", linkedinRoutes);
-
     const PORT = environment.port || 3000;
+
+    console.log("NODE_ENV:", process.env.NODE_ENV);
+    console.log("LINKEDIN_CLIENT_ID:", process.env.LINKEDIN_CLIENT_ID);
+    console.log("LINKEDIN_CLIENT_SECRET:", process.env.LINKEDIN_CLIENT_SECRET);
+    console.log("LINKEDIN_REDIRECT_URI:", process.env.LINKEDIN_REDIRECT_URI);
 
     app.listen(PORT, () => {
       console.log("=== Servidor Iniciado ===");
@@ -47,5 +54,9 @@ const startServer = async (): Promise<void> => {
 };
 
 startServer();
+
+app.use((_req, res) => {
+  res.status(404).send('Rota não encontrada.'); // Mensagem para rotas não definidas
+});
 
 export default app;
